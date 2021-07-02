85002d01061e467fd12f3c1348c266591870a8e3
Gabriel <gla280900@gmail.com>
2021-07-01 23:28:48 -0500

    Initial commit

---
+++ mysite/main/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.text
+
+
+# Aqui debo de poner mis 5 tablas (proveedores, productos, ventas, productos_proveedores, productos_ventas)
